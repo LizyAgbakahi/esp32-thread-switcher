193f3945f234ec4c04ab033111fe05dd7f841f23
LizyAgbakahi <lagbakahi@luc.edu>
2025-11-17 17:42:07 -0600

    Milestone 1: Basic TaskA/TaskB FreeRTOS thread switching demo

--- main/thread_switcher.c
+++ main/thread_switcher.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void taskA(void *param)
+{
+    while (1)
+    {
+        printf("Running Task A\n");
+        vTaskDelay(500 / portTICK_PERIOD_MS); // 500 ms delay
+    }
+}
+
+void taskB(void *param)
+{
+    while (1)
+    {
+        printf("Running Task B\n");
+        vTaskDelay(500 / portTICK_PERIOD_MS); // 500 ms delay
+    }
+}
 
 void app_main(void)
 {
+    printf("Starting Thread Switcher Demo...\n");
 
+    // Create the two tasks
+    xTaskCreate(taskA, "TaskA", 2048, NULL, 1, NULL);
+    xTaskCreate(taskB, "TaskB", 2048, NULL, 1, NULL);
 }
